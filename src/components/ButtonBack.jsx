9a1d31f6eed0c1fc2fb9807502eca0b47d3595f1
Alaicry <alaicry99@gmail.com>
2023-05-30 22:47:26 +0500

    DeviceForm component layout

--- src/components/ButtonBack.jsx
+++ src/components/ButtonBack.jsx
@@ -6,7 +6,7 @@ const ButtonBack = () => {
 	const navigate = useNavigate();
 	return (
 		<button
-			className="absolute top-[12px] left-[200px] bg-white p-3 rounded-md hover:bg-slate-100 flex items-center justify-center gap-1 text-base transition-all ease-linear delay-[0.15s]"
+			className="absolute top-[12px] left-[5%] bg-white p-3 rounded-md hover:bg-slate-100 flex items-center justify-center gap-1 text-base transition-all ease-linear delay-[0.15s]"
 			onClick={() => navigate(-1)}
 		>
 			<IoIosArrowRoundBack className="w-[30px] h-[30px]" />
--- src/components/DeviceForm.jsx
+++ src/components/DeviceForm.jsx
@@ -1,25 +1,62 @@
 import React from "react";
-import { useForm } from "react-hook-form";
 import { useDispatch } from "react-redux";
-import Container from "./Container";
+import { IoCaretForwardOutline, IoCaretDownOutline } from "react-icons/io5";
 
 const DeviceForm = () => {
+	const [form, setForm] = React.useState(false);
 	const dispatch = useDispatch();
 
 	return (
-		<form>
-			<label>Производитель</label>
-			<input type="text" />
-			<label>Марка</label>
-			<input type="text" />
-			<label>Модель</label>
-			<input type="text" />
-			<label>Тип устройства</label>
-			<select name="">
-				<option value="sensor">Датчик</option>
-				<option value="terminal">Терминал</option>
-			</select>
-		</form>
+		<React.Fragment>
+			<button
+				className="flex items-center gap-1 font-medium mb-3"
+				onClick={() => setForm(!form)}
+			>
+				Открыть форму
+				{form ? (
+					<IoCaretDownOutline className="w-5 h-5" />
+				) : (
+					<IoCaretForwardOutline className="w-5 h-5" />
+				)}
+			</button>
+			{form && (
+				<form className="flex flex-col">
+					<div className="mb-4 p-4 bg-gray-100 rounded-md flex flex-col gap-4">
+						<div className="flex flex-col gap-2">
+							<label className="font-medium">Производитель</label>
+							<input type="text" className="h-10 rounded-md pl-[20px] focus:outline-zinc-400" />
+						</div>
+						<div className="flex flex-col gap-2">
+							<label className="font-medium">Марка</label>
+							<input type="text" className="h-10 rounded-md pl-[20px] focus:outline-zinc-400" />
+						</div>
+						<div className="flex flex-col gap-2">
+							<label className="font-medium">Модель</label>
+							<input type="text" className="h-10 rounded-md pl-[20px] focus:outline-zinc-400" />
+						</div>
+						<div className="flex flex-col gap-2">
+							<label className="font-medium">Тип устройства</label>
+							<select
+								name="deviceType"
+								defaultValue="terminal"
+								className="h-10 rounded-md pl-[20px]"
+							>
+								<option value="sensor">Датчик</option>
+								<option value="terminal">Терминал</option>
+							</select>
+						</div>
+					</div>
+					<div className="flex flex-col gap-2">
+						<button
+							type="submit"
+							className="text-center mx-auto py-3 px-3 bg-orange-200 rounded-md"
+						>
+							Добавить
+						</button>
+					</div>
+				</form>
+			)}
+		</React.Fragment>
 	);
 };
 
